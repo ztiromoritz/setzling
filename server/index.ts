af76d3d8e59630e41bd5c904f72528d8c624cf6a
Moritz Rebbert <moritz.rebbert@udo.edu>
2020-11-14 15:20:45 +0100

    WS path via .env

--- server/index.ts
+++ server/index.ts
@@ -6,31 +6,52 @@ import WebSocket from 'ws';
 import { GameClientHandler } from "./lib/game/GameClientHandler";
 import { GameStore } from "./lib/game/GameStore";
 import { InstanceService } from "./lib/service/InstanceService";
-
+import { stringReplace } from "string-replace-middleware";
 dotenv.config()
-const {SERVER_PORT, WS_PORT} = process.env;
+const {SERVER_PORT, WS_PORT, WS_BASE_URL} = process.env;
 const serverPort = Number.parseInt(SERVER_PORT || "7777");
 const wsPort = Number.parseInt(WS_PORT || "7778");
+const wsBaseUrl = WS_BASE_URL || `ws://localhost:${wsPort}`
 
 
+//
+// WebSocket
+//
 const wsServer = new WebSocket.Server({
   port: wsPort
 });
 console.log(`[ws]: Websocket Server is running at ws://localhost:${wsPort}`)
+console.log(`[ws]: Public Websocket url is: ${wsBaseUrl}`)
 
+
+
+//
+// Engine
+//
 const gameStore = new GameStore();
 const clientHandler = new GameClientHandler(wsServer, gameStore)
 gameStore.setClientNotifier(clientHandler);
 gameStore.init();
 
-const app = express();
-app.get('/', (req, res) => res.send('Setzling!'));
 
+//
+// HTTP-Server
+//
+const app = express();
+
+// REST-Controller
 const instanceService = new InstanceService(gameStore); // TODO: may be some dependency injection??
 app.use('/api/instances/', new InstanceController(instanceService).getRouter())
 
+// Static files
+app.use('/client/',stringReplace({
+  '##wsBaseUrl##': wsBaseUrl,
+}));
 app.use('/client',express.static('../client/'))
 
 app.listen(serverPort, () => {
   console.log(`[server]: Server is running at https://localhost:${serverPort}`);
 });
+
+
+
