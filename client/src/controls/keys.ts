f3a4bc0a37df762a48a098536a7e06731c9bbb91
Moritz Rebbert <moritz.rebbert@udo.edu>
2020-12-23 20:28:25 +0100

    WASD can be used as arrows

--- client/src/controls/keys.ts
+++ client/src/controls/keys.ts
@@ -18,26 +18,26 @@ export function initializeKeys(ws: WebSocket){
 
 
     document.addEventListener('keyup', (e) => {
-        if (e.code === "ArrowUp") {
+        if (e.code === "ArrowUp" || e.code == "KeyW" ) {
             arrows.up = false;
-        } else if (e.code === "ArrowDown") {
+        } else if (e.code === "ArrowDown" || e.code == "KeyS") {
             arrows.down = false;
-        } else if (e.code === "ArrowLeft") {
+        } else if (e.code === "ArrowLeft" || e.code == "KeyA") {
             arrows.left = false;
-        } else if (e.code === "ArrowRight") {
+        } else if (e.code === "ArrowRight" || e.code == "KeyD") {
             arrows.right = false;
         }
         sendControlUpdate();
     })
 
     document.addEventListener('keydown', (e) => {
-        if (e.code === "ArrowUp") {
+        if (e.code === "ArrowUp"  || e.code == "KeyW" ) {
             arrows.up = true;
-        } else if (e.code === "ArrowDown") {
+        } else if (e.code === "ArrowDown" || e.code == "KeyS" ) {
             arrows.down = true;
-        } else if (e.code === "ArrowLeft") {
+        } else if (e.code === "ArrowLeft" || e.code == "KeyA") {
             arrows.left = true;
-        } else if (e.code === "ArrowRight") {
+        } else if (e.code === "ArrowRight" || e.code == "KeyA") {
             arrows.right = true;
         }
         sendControlUpdate();
